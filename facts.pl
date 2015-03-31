2b8f2bda6862cfe65715742cfe3202cc579c9987
alistairyves <alistairyves@gmail.com>
2015-03-30 20:52:03 -0400

    finished rules

--- facts.pl
+++ facts.pl
@@ -22,33 +22,86 @@ state(alt_psi).
 state(safe_status).
 state(risk_assess).
 
+%superstates
+superstate(init,boot_hw).
+superstate(init,senchk).
+superstate(init,ready).
+superstate(init,psichk).
+superstate(init,tchk).
+superstate(monitoring,monidle).
+superstate(monitoring,regulate_environment).
+superstate(monitoring,lockdown).
+superstate(lockdown,prep_vpurge).
+superstate(lockdown,alt_temp).
+superstate(lcokdown,alt_psi).
+superstate(lockdown,safe_status).
+superstate(lockdown,risk_assess).
+
+%initial states
+initial_state(boot_hw,init).
+initial_state(monidle,monitoring).
+initial_state(prep_vpurge,lockdown).
+
+
 %transitions from 2.5
-transition(safe_shutdown,final,killswitch_triggered,_,_,_).
-transition(dormant,final,killswitch_triggered,_,_,_).
-transition(init,final,killswitch_triggered,_,_,_).
-transition(idle,final,killswitch_triggered,_,_,_).
-transition(error_diagnosis,final,killswitch_triggered,_,_,_).
-transition(monitoring,final,killswitch_triggered,_,_,_).
+transition(safe_shutdown,final,killswitch_triggered,null,null,null).
+transition(dormant,final,killswitch_triggered,null,null,null).
+transition(init,final,killswitch_triggered,null,null,null).
+transition(idle,final,killswitch_triggered,null,null,null).
+transition(error_diagnosis,final,killswitch_triggered,null,null,null).
+transition(monitoring,final,killswitch_triggered,null,null,null).
 
 %safe_shutdown
-transition(safe_shutdown,dormant,sleep,_,_,_).
+transition(safe_shutdown,dormant,sleep,null,null,null).
 
 %init
-transition(init,idle,init_ok,_,_,_).
-transition(init,error_diagnosis,init_crash,_,_,_).
-transition(init,error_diagnosis,init_err_msg,_,_,_).
+transition(init,idle,init_ok,null,null,null).
+transition(init,error_diagnosis,init_crash,null,init_err_msg,null).
+
+%inside init
+transition(boot_hw,senchk,hwok,null,null,null).
+transition(senchk,tchk,sen_ok,null,null,null).
+transition(tchk,psichk,t_ok,null,null,null).
+transition(psichk,ready,psi_ok,null,null,null).
 
 %error_diagnosis
-transition(error_diagnosis,idle,idle_rescue,_,_,_).
-transition(error_diagnosis,monitoring,moni_rescue,_,_,_).
+transition(error_diagnosis,idle,idle_rescue,null,null,null).
+transition(error_diagnosis,monitoring,moni_rescue,null,null,null).
+transition(error_diagnosis,init,retry_init,'retry<=3','retry++',null).
+transition(error_diagnosis,safe_shutdown,shutdown,'retry>3','retry=0',null).
 
 %idle
-transition(idle,monitoring,begin_monitoring,_,_,_).
-transition(idle,error_diagnosis,idle_crash,_,_,_).
-transition(idle,error_diagnosis,idle_err_msg,_,_,_).
+transition(idle,monitoring,begin_monitoring,null,null,null).
+transition(idle,error_diagnosis,idle_crash,idle_err_msg,null,null).
 
 %monitoring
-transition(monitoring,error_diagnosis,monitor_crash,'lockdown=false',_,_).
-transition(monitoring,error_diagnosis,moni_err_msg,_,_,_).
+transition(monitoring,error_diagnosis,monitor_crash,'lockdown=false',moni_err_msg,null).
+%inside monitoring
+transition(monidle,regulate_environment,no_contagion,null,null,null).
+transition(regulate_environment,monidle,after_100ms,null,null,null).
+transition(monidle,monidle,verify_contagion,null,set_contagion,null).
+transition(monidle,lockdown,contagion_alert,null,faculty_crit_mesg,'inlockdown=true').
+transition(lockdown,monidle,purge_succ,null,'inlockdown=false',null).
+transition(prep_vpurge,alt_temp,initiate_purge,null,lock_doors,null).
+transition(prep_vpurge,alt_psi,initiate_purge,null,lock_doors,null).
+transition(alt_temp,alt_temp,perform_alteration,null,null,null).
+transition(alt_psi,alt_psi,perform_alteration,null,null,null).
+transition(alt_temp,risk_assess,toyc_comp,null,null,null).
+transition(alt_psi,risk_assess,psiyc_comp,null,null,null).
+transition(risk_assess,risk_assess,evaluate_risk,null,set_risk,null).
+transition(risk_assess,prep_vpurge,risk_action,'risk>1',null,null).
+transition(risk_assess,safe_status,risk_action,'risk<=1',unlock_doors,null).
+transition(safe_status,exit,null,null,null,null).
+
+
+
+
+
+
+
+
+
+
+
 
 
--- rules.pl
+++ rules.pl
@@ -1,13 +1,28 @@
-is_loop(Event, Guard).
+%is_loop(Event, Guard).
 %succeeds by finding a loop edge. We assume that an edge
 %can be represented by a non-null event-guard pair.
-all_loops(Set).
-%succeeds by returning a set of all loop edges.
-is_edge(Event, Guard).% succeeds by finding an edge.
-size(Length).% succeeds by returning the size of the entire EFSM (given by the number
-%of its edges).
-is_link(Event, Guard).%succeeds by finding a link edge.
-all_superstates(Set).% succeeds by finding all superstates in the EFSM.
+is_loop(Event,Guard):-
+	transition(State,State,Event,Guard,_,_).
+%all_loops(Set).
+%succeeds by returning a set of all loop edges
+all_loops(Set):-
+	findall(loop(Event,Guard),is_loop(Event,Guard),L),
+	list_to_set(L,Set).
+%is_edge(Event, Guard).% succeeds by finding an edge.
+is_edge(Event,Guard):-
+	transition(_,_,Event,Guard,_,_).
+% size(Length).% succeeds by returning the size of the entire EFSM (given
+% by the number of its edges).
+size(Length):-
+	findall(transition(State1,State2,Event,Guard,_,_),transition(State1,State2,Event,Guard,_,_),L),
+	length(L,Length).
+%is_link(Event, Guard).%succeeds by finding a link edge.
+is_link(Event,Guard):-
+	transition(_,_,Event,Guard,_,_).
+%all_superstates(Set).% succeeds by finding all superstates in the EFSM.
+all_superstates(Set):-
+	findall(State,superstate(State,_),L),
+	list_to_set(L,Set).
 ancestor(Ancestor, Descendant).% is a utility rule that succeeds by returning an ancestor
 %to a given state.
 inheritss_transitions(State, List). succeeds by returning all transitions inherited
